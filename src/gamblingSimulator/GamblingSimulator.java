88d6a6817b83a8a692d78297c356e72cfd97a8b0
vinit <vinitsxn@gmail.com>
2021-12-29 08:31:36 +0530

    Added: Welcome Message added

---
+++ src/gamblingSimulator/GamblingSimulator.java
@@ -0,0 +1,12 @@
+/**
+ * @author Vinit Kumar
+ * @date 29/12/2021
+ * Divide Rs N into 1,2,5,10,50,100,500,1000 Rs such that minimum no of notes returned
+ */
+package gamblingSimulator;
+
+public class GamblingSimulator {
+    public static void main(String[] args){
+        System.out.println("Welcome to Gambling Simulator:");
+    }
+}
